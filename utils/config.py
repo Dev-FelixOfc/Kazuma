9b4246678afc9a4b6fc1165b8e40ab638f4de23a
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-15 16:29:07 -0400

    Crear utils/config.py

---
+++ utils/config.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+JSON_PATH = os.path.join(os.path.dirname(__file__), "../../config.json")
+
+def get_json_data():
+    if os.path.exists(JSON_PATH):
+        with open(JSON_PATH, "r") as f:
+            return json.load(f)
+    return {}
+
+json_data = get_json_data()
+
+CONFIG = {
+    "bot_name": "Kazuma",
+    "owner": "5353448202",
+    "prefix": "/",
+    "phone_number": json_data.get("phone_number", ""),
+    "password": json_data.get("password", "")
+}
